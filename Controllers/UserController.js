8b3c27c64572da39db10e65b3330fa0dbf23e1ef
Meep220 <143765941+Meep220@users.noreply.github.com>
2024-11-24 21:32:51 +0800

    Commit 1: Added Empty Functions to fill out for features in user.

---
+++ Controllers/UserController.js
@@ -0,0 +1,18 @@
+
+// Possible Functions we will need for the Application
+const createUser = (req,res) =>{
+    
+}
+
+const userLogin = (req,res) =>{
+
+}
+// not sure if this should be in user or in task
+const createGroup = (req,res) =>{
+
+}
+
+const getUserProfile = (req,res) =>{
+
+}
+
---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 34612
+
+
+
+app.listen(port,()=>{
+    console.log(`listening to port ${port}`)
+})
